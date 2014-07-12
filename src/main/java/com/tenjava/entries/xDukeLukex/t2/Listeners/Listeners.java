4719026e7f25149d1ee19f941a551df4b978f649
xDukeLukex <luke.turner@live.nl>
2014-07-12 17:35:45 +0100

    Last Edition (RELEASE!!!!!)

--- src/main/java/com/tenjava/entries/xDukeLukex/t2/Listeners/Listeners.java
+++ src/main/java/com/tenjava/entries/xDukeLukex/t2/Listeners/Listeners.java
@@ -67,7 +67,7 @@ public class Listeners implements Listener {
 
         if (event.getPlayer().getLevel() >= 1) {
             new Shot(event.getPlayer().getEyeLocation(), event.getPlayer().getTargetBlock(null, 100).getLocation(), event.getPlayer());
-            event.getPlayer().setLevel(event.getPlayer().getTotalExperience() - 10 / 100000);
+            event.getPlayer().setLevel(1);
         } else {
             event.getPlayer().sendMessage(TenJava.getInstance().prefix + ChatColor.RED + "You do not have enough NRG!" + " You need " + ChatColor.YELLOW + String.valueOf(10 - event.getPlayer().getExp()) + ChatColor.RED + " more!");
             event.getPlayer().playSound(event.getPlayer().getEyeLocation(), Sound.CLICK, 1, 1);
