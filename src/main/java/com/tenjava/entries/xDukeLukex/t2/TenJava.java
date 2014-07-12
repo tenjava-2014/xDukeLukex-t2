8b52768e9f09eb5b267616fd77d8801cca724a8d
xDukeLukex <luke.turner@live.nl>
2014-07-12 12:51:34 +0100

    Worked on more basic minigame mechanism stuff.

--- src/main/java/com/tenjava/entries/xDukeLukex/t2/TenJava.java
+++ src/main/java/com/tenjava/entries/xDukeLukex/t2/TenJava.java
@@ -2,6 +2,7 @@ package com.tenjava.entries.xDukeLukex.t2;
 
 import com.tenjava.entries.xDukeLukex.t2.Listeners.Listeners;
 import com.tenjava.entries.xDukeLukex.t2.Util.GameState;
+import org.bukkit.ChatColor;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -10,6 +11,7 @@ public class TenJava extends JavaPlugin {
     private static TenJava instance;
     private PluginManager pm;
     private GameState gameState;
+    public String prefix = ChatColor.DARK_GRAY + "[" + ChatColor.GOLD + "Xenn" + ChatColor.DARK_GRAY + "]: " + ChatColor.RESET;
 
     @Override
     public void onEnable() {
