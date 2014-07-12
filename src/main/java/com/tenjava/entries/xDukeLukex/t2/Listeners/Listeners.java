36bcd975285a52ee599a63a90c2df61038ec5c33
xDukeLukex <luke.turner@live.nl>
2014-07-12 10:12:04 +0100

    Worked on the basic stuff in the JavaPlugin class.

---
+++ src/main/java/com/tenjava/entries/xDukeLukex/t2/Listeners/Listeners.java
@@ -0,0 +1,10 @@
+package com.tenjava.entries.xDukeLukex.t2.Listeners;
+
+import org.bukkit.event.Listener;
+
+/**
+ * Created by Luke on 12/07/2014.
+ */
+public class Listeners implements Listener {
+
+}
--- src/main/java/com/tenjava/entries/xDukeLukex/t2/TenJava.java
+++ src/main/java/com/tenjava/entries/xDukeLukex/t2/TenJava.java
@@ -1,5 +1,30 @@
 package com.tenjava.entries.xDukeLukex.t2;
 
+import com.tenjava.entries.xDukeLukex.t2.Listeners.Listeners;
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class TenJava extends JavaPlugin {}
+public class TenJava extends JavaPlugin {
+
+    private static TenJava instance;
+    private PluginManager pm;
+
+    @Override
+    public void onEnable() {
+        pm = getServer().getPluginManager();
+
+        pm.registerEvents(new Listeners(), this);
+
+        instance = this;
+    }
+
+    @Override
+    public void onDisable() {
+        instance = null;
+        pm = null;
+    }
+
+    public static TenJava getInstance(){
+        return instance;
+    }
+}
